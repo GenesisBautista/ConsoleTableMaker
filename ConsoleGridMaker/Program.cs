bf2a8e1d8ab17d0ddb3f7e843e90c4d6815a816d
GenesisBautista <genesis@bautista.link>
2023-05-14 23:53:10 -0700

    added more examples

--- ConsoleGridMaker/Program.cs
+++ ConsoleGridMaker/Program.cs
@@ -14,10 +14,13 @@ namespace Examples
             SimpleExample();
             FormattingExample();
             ColorExample();
+            AlignmentExamples();
+            ExtraExamples();
         }
 
         /// <summary>
         /// You can add headers and it will seperate the headers from the table data
+        /// padding can be added to the left and right
         /// </summary>
         static private void SimpleExample()
         {
@@ -41,6 +44,9 @@ namespace Examples
                 "Phone Number",
                 "Street Address"
             });
+
+            table.PaddingLeft = 1;
+            table.PaddingRight = 1;
 
             table.DrawTable();
         }
@@ -73,8 +79,9 @@ namespace Examples
         }
 
         /// <summary>
-        /// Text colors can be set by headers, table, column, or individual cell. 
-        /// headers will always be colored by HeaderColor property you cannot change header color by cell color or by column color
+        /// Border, headers, column, cell, and two data colors can be set. 
+        /// headers will always be colored by HeaderColor property you cannot change header color by cell color or by column color.
+        /// Data color takes two colors for alternating colors on the data.
         /// for data cells the hierarchy goes cell -> column -> table and it doesnt matter what order you set them in it will always be showing cell first.
         /// </summary>
         static void ColorExample()
@@ -108,6 +115,77 @@ namespace Examples
             table[8][5].Color = Color.LightGreen;
 
             table.DrawTable();
+        }
+
+        /// <summary>
+        /// Alignment follows the same rules as color cell -> column -> table
+        /// </summary>
+        static void AlignmentExamples()
+        {
+            Table table = new Table
+            {
+                new Row { "AMZN", "buy ", 556411.52, new DateTime(2021, 03, 16) },
+                new Row { "TSLA", "sell", 560522.35, new DateTime(2021, 10, 23) },
+                new Row { "FB  ", "sell", 863372.18, new DateTime(2021, 10, 14) },
+                new Row { "AAPL", "buy ", 755760.92, new DateTime(2021, 09, 23) },
+                new Row { "TSLA", "buy ", 303434.23, new DateTime(2021, 05, 08) },
+                new Row { "FB  ", "buy ", 898594.78, new DateTime(2021, 01, 17) },
+                new Row { "TSLA", "sell", 295381.26, new DateTime(2021, 07, 30) },
+                new Row { "AMZN", "sell", 593011.65, new DateTime(2021, 11, 26) },
+                new Row { "AAPL", "sell", 523493.86, new DateTime(2021, 04, 14) },
+                new Row { "FB  ", "sell", 881878.31, new DateTime(2021, 07, 28) }
+            };
+
+            table.AddHeaders(
+                new Row { "stock symbol", "trade type", "price", "trade date" }
+            );
+
+            table.Alignment = Align.Right;
+            table.AlignColumnAt(1, Align.Left);
+            table[2][1].Alignment = Align.Right;
+            table[3][1].Alignment = Align.Right;
+            table[7][1].Alignment = Align.Right;
+            table[8][1].Alignment = Align.Right;
+            table[9][1].Alignment = Align.Right;
+            table[10][1].Alignment = Align.Right;
+
+            table.DrawTable();
+        }
+
+        /// <summary>
+        /// Full grid allows your table to show borders between data cells
+        /// intelligibleRows can be turned off so that alternating colors will not happen. This will set the first color of the DataColors, the color hierarchy is still maintained.
+        /// </summary>
+        static void ExtraExamples()
+        {
+            Table table = new Table {
+                new Row { "Freemon","Cranmore","fcranmore1@webs.com","560-975-1941","8977 Northland Hill" },
+                new Row { "Dion","Feldman","dfeldman2@yellowbook.com","172-904-6740","652 Everett Avenue" },
+                new Row { "Giffie", "Attawell", "gattawell3@bravesites.com", "386-317-5262", "96964 Randy Circle" },
+                new Row { "Viv", "Wilman", "vwilman4@skype.com", "118-344-7212", "57 Bunker Hill Pass" },
+                new Row { "Stephanus", "Crumby", "scrumby5@sciencedaily.com", "459-297-4008", "45735 Ronald Regan Road" },
+                new Row { "Robenia", "Silmon", "rsilmon6@delicious.com", "396-793-7999", "34 Gale Pass" },
+                new Row { "Alicia", "Wenman", "awenman7@nature.com", "678-381-8855", "245 Upham Street" },
+                new Row { "Carlin", "Darlington", "cdarlington8@state.gov", "198-588-9032", "84 Sachs Junction" },
+                new Row { "Rivalee","Dodgshon","rdodgshon9@examiner.com","606-575-3779","230 Golf Circle"}
+            };
+
+            table.AddHeaders(new Row()
+            {
+                "First Name",
+                "Last Name",
+                "Email",
+                "Phone Number",
+                "Street Address"
+            });
+
+            table.PaddingLeft = 1;
+            table.PaddingRight = 1;
+            table.FullGrid = true;
+            table.IntelligibleRows = false;
+            table.SetColumnColorAt(2, Color.Gray);
+
+            Console.WriteLine(table.ToString());
         }
     }
 }
